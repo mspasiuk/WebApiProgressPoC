b0c2e73c9037868530ef82ca76cd905914e309cc
Martin Spasiuk <mspasiuk@gmail.com>
2016-03-14 20:18:01 -0300

    Cambio el task controller para devolver status 204

--- WebApiProgressPoc/Controllers/TaskController.cs
+++ WebApiProgressPoc/Controllers/TaskController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 using WebApiProgressPoc.Services;
 
@@ -14,31 +15,35 @@ namespace WebApiProgressPoc.Controllers
         [HttpGet]
         public void LongTask(string id)
         {
-            var p = new ProgressHub();
-            p.NotifyProgress(id,0);
+            Task.Run(() =>
+                {
+                    var p = new ProgressHub();
+                    p.NotifyProgress(id, 0);
 
-           
-            Thread.Sleep(2000);
-            p.NotifyProgress(id, 20);
 
-            
-            Thread.Sleep(1000);
-            p.NotifyProgress(id, 30);
+                    Thread.Sleep(2000);
+                    p.NotifyProgress(id, 20);
 
-            
-            Thread.Sleep(2000);
-            p.NotifyProgress(id, 50);
 
-            
-            Thread.Sleep(3000);
-            p.NotifyProgress(id, 80);
+                    Thread.Sleep(1000);
+                    p.NotifyProgress(id, 30);
 
-            
-            Thread.Sleep(1000);
-            p.NotifyProgress(id, 90);
 
-            Thread.Sleep(1000);
-            p.NotifyProgress(id,100);
+                    Thread.Sleep(2000);
+                    p.NotifyProgress(id, 50);
+
+
+                    Thread.Sleep(3000);
+                    p.NotifyProgress(id, 80);
+
+
+                    Thread.Sleep(1000);
+                    p.NotifyProgress(id, 90);
+
+                    Thread.Sleep(1000);
+                    p.NotifyProgress(id, 100);
+                }
+            );
         }
     }
 }
